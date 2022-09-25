29a716d0ca3ff947447778f445d19c139d3d5245
JohaanMannanal <johaan.joseph@gmail.com>
2022-09-25 08:28:03 -0700

    Added a Read More for Mentors Contact

--- public/index.js
+++ public/index.js
@@ -89,3 +89,35 @@ document.querySelectorAll("[data-copy]").forEach((el) => {
     });
   });
 });
+
+function readMoreAction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readMore");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function readMoreAction2() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("readMore2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "Read more";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "Read less";
+    moreText2.style.display = "inline";
+  }
+}
